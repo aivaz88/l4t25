a0ea810647010a38f28b31bef3b6e99a95c41eac
aivaz88 <aivaz1988@mail.ru>
2022-09-29 12:47:41 +0500

    Functions

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Введите число, которое нужно возвести в степень: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число, на какую степень нужно возвести: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"{Power(num1, num2)}");
+
+int Power(int x, int y)
+{
+    int sqr_num = 1;
+    for (int i = 0; i < y; i++)
+    {
+        sqr_num = sqr_num * x;
+    }
+    return(sqr_num);
+}
